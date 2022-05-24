1f0a016e3806ede4e95a5ee60b3a4e818fdf9b18
Quarkhopper <quarkhopper@gmail.com>
2022-05-23 20:12:20 -0700

    Increased the impulse constant for more exciting debris ejection by default.

--- script/GameOptions.lua
+++ script/GameOptions.lua
@@ -330,7 +330,7 @@ function create_mode_option_set(name, display_name)
 
 	oSet.impulse_const = create_mode_option(
 		option_type.numeric, 
-		80,
+		500,
 		"impulse_const",
 		"Impulse (pushing) constant")
 	oSet.impulse_const.range.lower = 0
