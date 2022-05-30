0d30a81e5791985e07dbfc8bfcfcb17b1ddb9192
Quarkhopper <quarkhopper@gmail.com>
2022-05-30 15:25:21 -0700

    Thrower splash

--- script/Thrower.lua
+++ script/Thrower.lua
@@ -3,13 +3,13 @@
 
 fireballs = {}
 
-
 function inst_fireball(pos, dir)
     local inst = {}
     inst.pos = pos
     inst.dir = dir
     inst.speed = TOOL.THROWER.speed.value
     inst.dist_left = TOOL.THROWER.max_dist.value
+    inst.splits_remaining = 4
     return inst
 end
 
@@ -30,17 +30,26 @@ function thrower_tick(dt)
 		if fireball.dist_left > 0 then 
 			local hit, dist, normal = QueryRaycast(fireball.pos, fireball.dir, fireball.speed + 0.1, 0.025)
             if hit then 
-                local new_dirs = radiate(fireball.dir, 90, 4)
+                -- hit something - break apart
+                local new_dirs = radiate(fireball.dir, 45, fireball.splits_remaining)
                 for j = 1, #new_dirs do
-                    apply_force(TOOL.THROWER.pyro.ff, fireball.pos, VecScale(new_dirs[j], TOOL.THROWER.pyro.ff.f_max))
+                    local new_fireball = inst_fireball(fireball.pos, new_dirs[j])
+                    new_fireball.dist_left = 2
+                    new_fireball.speed = 1
+                    new_fireball.splits_remaining = fireball.splits_remaining - 1
+                    table.insert(fireballs_next_tick, new_fireball) 
+                    -- apply_force(TOOL.THROWER.pyro.ff, fireball.pos, VecScale(new_dirs[j], TOOL.THROWER.pyro.ff.f_max))
                 end
             else
+                -- continued flight
                 fireball.dir = VecNormalize(VecAdd(fireball.dir, Vec(0, -TOOL.THROWER.gravity.value, 0)))
                 local advance = VecScale(fireball.dir, fireball.speed)
                 fireball.pos = VecAdd(fireball.pos, advance)
                 fireball.dist_left = fireball.dist_left - fireball.speed
                 local point_force = VecScale(fireball.dir,  TOOL.THROWER.pyro.ff.f_max) 
                 apply_force(TOOL.THROWER.pyro.ff, fireball.pos, point_force)
+                -- DebugCross(fireball.pos)
+                -- DebugLine(fireball.pos, VecAdd(fireball.pos, VecScale(fireball.dir, fireball.speed)))
                 table.insert(fireballs_next_tick, fireball)
             end
 		end
