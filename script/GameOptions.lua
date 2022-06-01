012db84a1d3011c2a04a507dc007286cae879437
Quarkhopper <quarkhopper@gmail.com>
2022-05-31 22:31:50 -0700

    upped rocket spread to 15 degs

--- script/GameOptions.lua
+++ script/GameOptions.lua
@@ -505,7 +505,7 @@ function create_rocket_option_set()
 	oSet.prop_decay.value = 0.2
 	oSet.heat_rise.value = 0.4
 	oSet.point_split.value = 3
-	oSet.extend_spread.value = 10
+	oSet.extend_spread.value = 15
 	oSet.flame_dead_force.value = 0.6
 	oSet.min_smoke_size.value = 0.3
 	oSet.max_smoke_size.value = 0.6
