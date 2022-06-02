b9c1bfec0c589578e8a90249cb82d2fbbb863f55
Quarkhopper <quarkhopper@gmail.com>
2022-06-01 20:41:52 -0700

    Player push now takes into account impulse constant so turning it off has an effect for the flamethrower

--- script/PyroField.lua
+++ script/PyroField.lua
@@ -266,7 +266,7 @@ function impulse_fx(pyro)
             end
         end
         if VecLength(VecSub(player_trans.pos, point.pos)) <= pyro.impulse_radius then
-            SetPlayerVelocity(VecAdd(GetPlayerVelocity(), VecScale(point.vec, 0.5)))
+            SetPlayerVelocity(VecAdd(GetPlayerVelocity(), VecScale(force_dir, force_n^2 * pyro.impulse_const * 0.005)))
         end
     end
 end
