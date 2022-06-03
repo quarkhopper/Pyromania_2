d4f733bd5ffda6ce66f1578467da635157976227
Quarkhopper <quarkhopper@gmail.com>
2022-06-02 19:14:11 -0700

    Embersgo back to starting at smoke size 1 down to 0.1 for the flameout effect. Jitter on embers set back to 0.5 or resolution.

--- script/PyroField.lua
+++ script/PyroField.lua
@@ -153,9 +153,9 @@ function make_flame_effect(pyro, flame, dt)
         smoke_size = fraction_to_range_value(flame.life_n, pyro.max_smoke_size, pyro.min_smoke_size)
     else
         local afterlife_n = range_value_to_fraction(flame.parent.mag, pyro.ff.f_dead, pyro.flame_dead_force)
-        smoke_size = fraction_to_range_value(afterlife_n, 0.1, pyro.max_smoke_size)
+        smoke_size = fraction_to_range_value(afterlife_n, 0.1, 1) -- pyro.max_smoke_size)
         -- Jitter is added to an ember to simulate flutter. Ok, it just looks better to me.
-        local jitter = random_vec(pyro.ff.resolution * 0.2)
+        local jitter = random_vec(pyro.ff.resolution * 0.5)
         flame.pos = VecAdd(flame.pos, jitter)
     end
     -- finish setting the fire puff particle params
