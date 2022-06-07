6c44ff449e6cef7723db0907c2ca1dc981095001
Quarkhopper <quarkhopper@gmail.com>
2022-06-07 14:59:12 -0700

    Adjusted ember puff values to be a bit light

--- script/PyroField.lua
+++ script/PyroField.lua
@@ -147,7 +147,7 @@ function make_flame_effect(pyro, flame, dt)
         particle_size = fraction_to_range_value(flame.life_n, pyro.max_smoke_size, pyro.min_smoke_size)
     else
         local afterlife_n = range_value_to_fraction(flame.parent.mag, pyro.ff.f_dead, pyro.flame_dead_force)
-        puff_value = range_value_to_fraction(flame.parent.mag, pyro.ff.f_dead, pyro.flame_dead_force)
+        puff_value = (range_value_to_fraction(flame.parent.mag, pyro.ff.f_dead, pyro.flame_dead_force) * 0.9) + 0.1
         particle_size = fraction_to_range_value(afterlife_n, 0.2, pyro.max_smoke_size)
         intensity = fraction_to_range_value(afterlife_n, 0.2, intensity)
         -- Jitter is added to an ember to simulate flutter. this prevents the smaller sized particules from 
